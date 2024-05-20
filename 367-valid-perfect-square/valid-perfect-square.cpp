c3579006db34bc9589daa3534056c8982316c51f
Anuradha Naidu <88324015+Anuradha-Naidu@users.noreply.github.com>
2024-05-20 14:31:30 +0530

    Time: 0 ms (100.00%) | Memory: 7.2 MB (12.91%) - LeetSync

---
+++ 367-valid-perfect-square/valid-perfect-square.cpp
@@ -0,0 +1,29 @@
+#define li long int
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        
+        //li = long int
+
+        li high = 100000;
+        li low = 0;
+        li mid;
+        li square;
+
+        while(low <= high){
+
+            mid = low + (high-low)/2;
+            square = mid*mid;
+            if(square == num)
+                return true;
+            
+            else if(square < num)
+                low = mid+1;
+            
+            else 
+                high = mid - 1;
+            
+        }
+        return false;
+    }
+};
